858ce20910c69c9c1a756630589d99d670d5e4d4
tikodjati <tikodjati@gmail.com>
2026-01-03 19:55:38 +0700

    Mejawab soal1 dan soal2 part3

---
+++ quiz/part3/soal3.js
@@ -0,0 +1,13 @@
+//tentukan apakah ini deret aritmatika atau bukan
+function tentukanDeretAritmatika(arr) {
+  // you can only write your code here
+    let selisih1 = arr[1] - arr[0];
+     
+}
+
+// TEST CASES
+console.log(tentukanDeretAritmatika([1, 2, 3, 4, 5, 6])); // true
+console.log(tentukanDeretAritmatika([2, 4, 6, 12, 24])); // false
+console.log(tentukanDeretAritmatika([2, 4, 6, 8])); // true
+console.log(tentukanDeretAritmatika([2, 6, 18, 54])); // false
+console.log(tentukanDeretAritmatika([1, 2, 3, 4, 7, 9])); // false
